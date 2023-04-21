724544432989188e19c3596bd0483b1ec4be3e2a
Yavor99 <yavor_enchev99@abv.bg>
2023-04-21 20:09:17 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Product from './components/Product';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-     
+     <Header />
+      <Routes>
+
+      <Route path='/' element={<Product />}/>
+
+
+      </Routes>
+      
     </div>
+    </BrowserRouter>
   );
 }
 
